640d551cd7cc4e5b00570cfd2cf1b97eaa606de1
osaro123 <chibueze.k.muoneke@gmail.com>
2024-03-19 08:14:57 +0100

    added chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,10 @@
+import Calendar from './Calendar'
 import React from 'react'
 
 const Chat = () => {
   return (
     <div className='md:pl-[16rem] mx-auto pt-8 w-[90%]'>
-        <p>Chat</p>
+        <Calendar/>
     </div>
   )
 }
